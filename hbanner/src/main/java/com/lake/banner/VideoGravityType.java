cee8cf1c2a66ba0d6e92c9eb7f02af94d5d0085a
lake <huboa1234@126.com>
2020-06-04 11:26:43 +0800

    解决竖屏切换时，视频相邻图片闪烁问题

--- hbanner/src/main/java/com/lake/banner/VideoGravityType.java
+++ hbanner/src/main/java/com/lake/banner/VideoGravityType.java
@@ -1,6 +1,6 @@
 package com.lake.banner;
 
-public class BannerGravity {
+public class VideoGravityType {
     public static final int CENTER = 0;
     public static final int CENTER_HORIZONTAL = 1;
     public static final int FULL_SCREEN = 2;
